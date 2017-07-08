401e50203f4f5d605fc98c16bc9bca1ea5bfee06
兵藤允彦 <masahikohyodo@masa.local>
2017-07-08 22:15:13 +0900

    Initial Commit

---
+++ firstErrand_childTests/firstErrand_childTests.swift
@@ -0,0 +1,36 @@
+//
+//  firstErrand_childTests.swift
+//  firstErrand_childTests
+//
+//  Created by 兵藤允彦 on 2017/07/08.
+//  Copyright © 2017年 funkey. All rights reserved.
+//
+
+import XCTest
+@testable import firstErrand_child
+
+class firstErrand_childTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
